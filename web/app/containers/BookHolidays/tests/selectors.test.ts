119984b1e8a208dededf69dd1f0f85a6306e3d60
im-mk <kaleem83@gmail.com>
2020-02-15 21:39:46 +0000

    Removed web folder

--- web/app/containers/BookHolidays/tests/selectors.test.ts
+++
@@ -1,7 +0,0 @@
-// import { selectBookHolidaysDomain } from '../selectors';
-
-describe('selectBookHolidaysDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
-  });
-});
